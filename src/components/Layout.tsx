937307b9e93bf5556d7da51487395964967305e1
Bernardo Garces Chapero <bernardo@bitbolic.com>
2024-08-07 21:58:04 +0100

    fix lock file

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,13 +3,10 @@ import React from "react";
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
-    ? () => null // Render nothing in production
+    ? () => null
     : React.lazy(() =>
-        // Lazy load in development
         import("@tanstack/router-devtools").then((res) => ({
           default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel
         })),
       );
 
